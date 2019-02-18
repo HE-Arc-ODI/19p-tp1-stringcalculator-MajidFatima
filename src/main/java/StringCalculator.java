1fa637b05c3eaaea9120383bc06e9ba00fe1b26a
MajidFatima <fatima.majid@he-arc.ch>
2019-02-18 22:09:25 +0100

    Update StringCalculator.java
    Refactoring

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,15 @@
+import static java.lang.Integer.*;
+
 public class StringCalculator {
     public static int add(String s) {
         if (s.isEmpty()) {
             return 0;
         } else if(s.contains(",")) {
             String[] tokens = s.split(",");
-            return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+            return parseInt(tokens[0]) + parseInt(tokens[1]);
         }else
          {
-            return Integer.parseInt(s);
+            return parseInt(s);
         }
     }
 }
