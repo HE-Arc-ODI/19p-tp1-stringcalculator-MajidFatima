a14625410ee1a17e7df40b912d27e4337faca8fb
fatima.majid <fatima.majid@he-arc.ch>
2019-02-18 21:07:14 +0100

    Update StringCalculatorTest.java
    Write a test and watch it fail.
    
    Empty string

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
+
+    @Before
+    public void setup(){
+    }
+
+    @Test
+    public void EmptyString() {
+        assertEquals(0, StringCalculator.add(""));
+    }
 }
