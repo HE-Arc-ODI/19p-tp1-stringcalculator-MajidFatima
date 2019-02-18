2eca0b5fd93bdc2877026c0a167ae99da2611406
fatima.majid <fatima.majid@he-arc.ch>
2019-02-18 21:36:09 +0100

    Update StringCalculator.java
    testtwoString separated by comma passed

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,7 +2,11 @@ public class StringCalculator {
     public static int add(String s) {
         if (s.isEmpty()) {
             return 0;
-        } else {
+        } else if(s.contains(",")) {
+            String[] tokens = s.split(",");
+            return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+        }else
+         {
             return Integer.parseInt(s);
         }
     }
