1ac14e4b46f2be005b5e2e2898497c767a4a36a8
fatima.majid <fatima.majid@he-arc.ch>
2019-02-19 09:07:52 +0100

    Update StringCalculator.java
    testPass sumString

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,12 +1,18 @@
+import static java.util.Arrays.asList;
 
 public class StringCalculator {
-
+    private static int startIndex;
     public static int add(String s) {
         if (s.isEmpty()) {
             return 0;
         } else if(s.contains(",")) {
             String[] tokens = s.split(",");
-            return sum(tokens[0], tokens[1]);
+           int total = 0;
+           for (String value : tokens){
+               total += Integer.valueOf(value);
+           }
+            //return sum(tokens[0], tokens[1]);
+            return total;
         }else
          {
              return parseToInt(s);
