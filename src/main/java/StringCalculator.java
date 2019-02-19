60f6a5e3607298c287d6744f2542467f3870e4ba
MajidFatima <fatima.majid@he-arc.ch>
2019-02-20 00:04:21 +0100

    Update StringCalculator.java
    make negative numbers pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,12 +1,14 @@
 import java.util.Arrays;
 import java.util.List;
 
+
 public class StringCalculator {
     public static int add(String s) {
         if (s.isEmpty()) {
             return 0;
         } else  {
             List<String> numbers = delimiters(s);
+
             return (int) sum(numbers);
         }
     }
@@ -22,12 +24,22 @@ public class StringCalculator {
     }
 
     private static int sum(List<String> tokens) {
+        String negatives = "";
+        for (String value : tokens){
+            if(parseToInt(value) <  0 ){
+                negatives+= ","+ value;
+            }
+        }
+        if(negatives.length() > 0 ){
+            throw new IllegalArgumentException("Negative number is not allowed : "+ negatives.substring(1));
+        }
         int total = 0;
         for (String value : tokens){
             total += Integer.valueOf(value);
         }
         return total;
     }
+
 
     private static int parseToInt(String s) {
         return Integer.parseInt(s);
