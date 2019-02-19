d32dc488cc479ea4ffc7c90b667f558a4c8850b1
fatima.majid <fatima.majid@he-arc.ch>
2019-02-19 07:55:06 +0100

    Update StringCalculator.java
    refractor Integer.parseInt to method

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,15 +1,18 @@
-import static java.lang.Integer.*;
-
 public class StringCalculator {
+
     public static int add(String s) {
         if (s.isEmpty()) {
             return 0;
         } else if(s.contains(",")) {
             String[] tokens = s.split(",");
-            return parseInt(tokens[0]) + parseInt(tokens[1]);
+            return parseToInt(tokens[0]) + parseToInt(tokens[1]);
         }else
          {
-            return parseInt(s);
-        }
+             return parseToInt(s); 
+         }
+    }
+
+    private static int parseToInt(String s) {
+        return Integer.parseInt(s);
     }
 }
