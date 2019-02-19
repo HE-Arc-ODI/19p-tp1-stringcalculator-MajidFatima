31aaf99bf65115df02d0eed778a57f1b6bcace8e
fatima.majid <fatima.majid@he-arc.ch>
2019-02-19 09:10:47 +0100

    Update StringCalculator.java
    refractor sum method

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -6,20 +6,19 @@ public class StringCalculator {
         if (s.isEmpty()) {
             return 0;
         } else if(s.contains(",")) {
-            String[] tokens = s.split(",");
-           int total = 0;
-           for (String value : tokens){
-               total += Integer.valueOf(value);
-           }
-            return total;
+            return sum(s.split(","));
         }else
          {
              return parseToInt(s);
          }
     }
 
-    public static int sum(String n1, String n2){
-        return parseToInt(n1) + parseToInt(n2);
+    private static int sum(String[] tokens) {
+        int total = 0;
+        for (String value : tokens){
+            total += Integer.valueOf(value);
+        }
+        return total;
     }
 
     private static int parseToInt(String s) {
