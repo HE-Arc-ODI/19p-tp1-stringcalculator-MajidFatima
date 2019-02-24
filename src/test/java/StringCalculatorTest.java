ff017e52bbb34e540c9f5d2cccaaa93a6f225b7d
MajidFatima <fatima.majid@he-arc.ch>
2019-02-24 22:18:44 +0100

    Update StringCalculatorTest.java
    add lastTest not pass

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -52,4 +52,9 @@ public class StringCalculatorTest {
     public void AllKindOfDelimiters(){
         assertEquals(6, StringCalculator.add("//[***]\n1***2***3"));
     }
+
+    @Test
+    public void  MultipleDelimiters(){
+        assertEquals(6, StringCalculator.add("//[*][%]\\n1*2%3"));
+    }
 }
